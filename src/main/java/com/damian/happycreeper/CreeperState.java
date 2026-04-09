07332446415af06fe2cb6162e04bdf86d8f1e7b9
Damian-Narcis <damianionut990@gmail.com>
2026-04-09 23:23:21 +0300

    Add biscuit-based creeper state system and creeper head protection

---
+++ src/main/java/com/damian/happycreeper/CreeperState.java
@@ -0,0 +1,39 @@
+package com.damian.happycreeper;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.monster.Creeper;
+
+public enum CreeperState {
+    NORMAL("normal"),
+    WEAKENED("weakened"),
+    TAMED("tamed");
+
+    private static final String STATE_TAG = "HappyCreeperState";
+
+    private final String serializedName;
+
+    CreeperState(String serializedName) {
+        this.serializedName = serializedName;
+    }
+
+    public static CreeperState get(Creeper creeper) {
+        CompoundTag data = creeper.getPersistentData();
+        String value = data.getString(STATE_TAG);
+
+        for (CreeperState state : values()) {
+            if (state.serializedName.equals(value)) {
+                return state;
+            }
+        }
+
+        return NORMAL;
+    }
+
+    public static void set(Creeper creeper, CreeperState state) {
+        creeper.getPersistentData().putString(STATE_TAG, state.serializedName);
+    }
+
+    public boolean isAtLeastWeakened() {
+        return this == WEAKENED || this == TAMED;
+    }
+}
